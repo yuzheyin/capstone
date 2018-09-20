71ebe597c43513ef2e37867b930facf1297610a6
Yuzhe Yin <yuzhey@andrew.cmu.edu>
2018-09-20 17:45:32 -0400

    OAuth

--- server/index.js
+++ server/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
-const app = express();
-//create a rout handler(app.post, app.put, app.delete)
-app.get('/', (req, res) => {
-  res.send({hi: 'there'});
-});
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
-const PORT = process.env.PORT || 5000;
+const app = express();
+
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+},
+(accessToken, refreshToken, profile, done) =>{
+  console.log(accessToken);
+  console.log('refresh token', refreshToken);
+  console.log('profile:', profile);
+}));
+
+app.get('/auth/google', passport.authenticate('google',{
+  scope: ['profile', 'email']
+}))
+
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+const PORT = process.env.PORT || 5000
 app.listen(PORT);
